2051d4729c5be776cb4f16da8cfe2682e54f1427
Pratik Jadhav <psj334061236@gmail.com>
2022-08-28 15:46:27 +0530

    Started Leaning new day

---
+++ _28-08-2022/src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args){
+        int v1 = 66;
+        int v2 = 55;
+        System.out.println("Addition "+ (v1+v2));
+        System.out.println("Substraction " + (v1-v2));
+        System.out.println("Multiplication "+v1*v2);
+        System.out.println("Division "+v1/v2);
+    }
+}
+
+//public class Variables {
+//    public static void main(String[] args){
+//         int value = 10;
+//         int value2 = 14*55;
+//         int sum = value+value2;
+//
+//        System.out.println(value);
+//        System.out.println(value2);
+//        System.out.println(sum);
+//    }
+//}
